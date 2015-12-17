79c6bc54fd2ce13d7392788ebf0826265d1d3d56
Waitaya Krongapiradee <wyte.sk133@gmail.com>
2015-12-17 11:23:36 -0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'cke#index'
+
+  post 'upload' => 'cke#upload'
+  post 'browse' => 'cke#browse'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
